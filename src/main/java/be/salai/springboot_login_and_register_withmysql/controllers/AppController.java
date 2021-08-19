08ee2bc4ca9984927e06970f79d49c5a7381030e
Salaiaung40 <salaihtunlin@hotnail.com>
2021-08-19 11:57:55 +0200

    Complete Register From and landing page.

--- src/main/java/be/salai/springboot_login_and_register_withmysql/controllers/AppController.java
+++ src/main/java/be/salai/springboot_login_and_register_withmysql/controllers/AppController.java
@@ -1,11 +1,18 @@
 package be.salai.springboot_login_and_register_withmysql.controllers;
 
+import be.salai.springboot_login_and_register_withmysql.model.User;
+import be.salai.springboot_login_and_register_withmysql.repositories.UserRep;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private UserRep repo;
 
     @GetMapping("/")
     public String viewHomePage() {
@@ -13,15 +20,23 @@ public class AppController {
     }
 
     @GetMapping("/login")
-    public String viewLoginPage() {
+    public String viewLoginPage(Model model) {
+
         return "login";
     }
 
     @GetMapping("/register")
     public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
         return "register";
     }
 
+@PostMapping("/process_register")
+    public  String processRegistering (User user) {
+        repo.save(user);
 
+        return "registerSuccess";
+//        can be land direct to the profile setting page.
+}
 }
 
